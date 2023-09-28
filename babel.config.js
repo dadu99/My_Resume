3538838dc7dfe5ecee2766fee0805a7657453a02
Darius Baciu <baciu.darius01@gmail.com>
2023-09-28 15:09:27 +0300

    babel

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,9 @@
 module.exports = {
   presets: [
     [
-      "@vue/cli-plugin-babel/preset",
+      "@vue/app",
       {
-        targets: { esmodules: true },
-        polyfills: false,
+        polyfills: ["es.promise", "es.symbol"],
       },
     ],
   ],
