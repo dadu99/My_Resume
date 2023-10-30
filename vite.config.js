dc30750825e37a6a663b23ba7644aabaf896f3df
Darius Baciu <baciu.darius01@gmail.com>
2023-10-30 13:11:21 +0200

    update vue version

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
 import vuetify from "vite-plugin-vuetify";
+import vue from "@vitejs/plugin-vue";
+
 export default defineConfig({
   plugins: [vue(), vuetify({ autoImport: true })],
   resolve: {
